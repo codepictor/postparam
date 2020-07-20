b0627401865871ee71016bf262f6f4a40d63c478
codepictor <grozzmaster@gmail.com>
2020-07-20 23:14:23 +0300

    Add motor

--- src/motor/admittance_matrix.py
+++ src/motor/admittance_matrix.py
@@ -3,9 +3,16 @@ import sympy
 
 class AdmittanceMatrix:
 
-    def __init__(self):
+    def __init__(self, L, R, J, K):
         """Define relations between input and output data."""
+        b = sympy.symbols('b', real=True)
+        omega = sympy.symbols('omega', real=True)
 
+        self._params = [b]
+        self._omega = omega
+        self._admittance_matrix = sympy.Matrix([
+            K / ((sympy.I * omega * L + R) * (b + sympy.I * omega * J) + K**2)
+        ])
 
     @property
     def params(self):
--- src/motor/dynamical_system.py
+++ src/motor/dynamical_system.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import scipy.signal
 
 from . import admittance_matrix
 
@@ -6,11 +8,46 @@ from . import admittance_matrix
 class DynamicalSystem:
 
     def generate_time_data(self):
+        b = self.true_params[0]
+        L = self._get_fixed_params()['L']
+        R = self._get_fixed_params()['R']
+        J = self._get_fixed_params()['J']
+        K = self._get_fixed_params()['K']
 
+        A = np.array([
+            [-b / J,  K / J],
+            [-K / L, -R / L]
+        ])
+        B = np.array([
+            [0],
+            [1 / L]
+        ])
+        C = np.array([
+            [1, 0]
+        ])
+        D = np.array([
+            [0]
+        ])
+        sys = sp.signal.StateSpace(A, B, C, D)
+
+        min_t = 0.0
+        max_t = 10.0
+        dt = 0.01
+        tin = np.arange(min_t, max_t, step=dt)
+
+        omega0 = 2 * np.pi * 10
+        V0 = 2
+        V = 12 + V0 * np.cos(omega0 * tin)
+        # V = 12 * np.ones(len(tin))
+
+        tout, yout, xout = sp.signal.lsim(sys, V, tin, X0=None)
+        assert (np.diff(tout) > dt * 99999999 / 100000000).all()
+        assert (np.diff(tout) < dt * 100000001 / 100000000).all()
+        assert np.array_equal(xout[:, 0], yout)
 
         return {
-            'inputs': ,
-            'outputs': ,
+            'inputs': np.array([V]),
+            'outputs': np.array([yout]),
             'dt': dt
         }
 
@@ -22,13 +59,21 @@ class DynamicalSystem:
         perturbed_params = (perturbations + 1.0) * self.true_params
         return perturbed_params
 
+    def _get_fixed_params(self):
+        return {
+            'L': 0.5,
+            'R': 1.0,
+            'J': 0.01,
+            'K': 0.01
+        }
+
     @property
     def true_params(self):
-        return np.array([])
+        return np.array([0.1])
 
     @property
     def param_uncertainty(self):
-        return np.array([])
+        return np.array([0.5])
 
     @property
     def system_name(self):
@@ -36,7 +81,7 @@ class DynamicalSystem:
 
     @property
     def params_names(self):
-        return []
+        return ['b']
 
     @property
     def min_freq(self):
@@ -44,9 +89,11 @@ class DynamicalSystem:
 
     @property
     def max_freq(self):
-        return 8.0
+        return 20.0
 
     @property
     def admittance_matrix(self):
-        return admittance_matrix.AdmittanceMatrix()
+        return admittance_matrix.AdmittanceMatrix(
+            **self._get_fixed_params()
+        )
 
